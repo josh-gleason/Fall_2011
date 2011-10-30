95a0d2054451c28e2ca8cdf93f691a4d29183356
josh <gleaso22@gmail.com>
2011-10-30 00:00:26 -0700

    added line count script in source directory

---
+++ CS480/Sketchpad/src/countlines.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+c=0
+for f in `find *`; do
+  if [ -f "$f" ]; then
+    x=`wc -l $f | tr " " "\n"`
+    for w in $x; do
+      if [ $w -eq $w 2> /dev/null ]; then
+        c=$(($c+$w))
+      fi
+    done
+  fi
+done
+echo "Total Line count is $c"
